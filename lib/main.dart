6abc773f49020541917f7b46a18f1cd1622adfd1
David <furia.designer@gmail.com>
2020-10-09 00:30:20 -0300

    update app

--- lib/main.dart
+++ lib/main.dart
@@ -20,18 +20,20 @@ class MyHomePage extends StatelessWidget {
         appBar: AppBar(
           title: Text('Despesas Pessoais'),
         ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Container(
-              child: Card(
-                child: Text('Gráfico'),
-                color: Colors.blue,
-                elevation: 5,
+        body: SingleChildScrollView(
+                  child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                child: Card(
+                  child: Text('Gráfico'),
+                  color: Colors.blue,
+                  elevation: 5,
+                ),
               ),
-            ),
-            TransactionUser(),
-          ],
+              TransactionUser(),
+            ],
+          ),
         ));
   }
 }
